f70fc0dfa2fd2d2280af81ede567fc0283b38e32
Trollerker <berserker1523@hotmail.com>
2025-11-03 00:38:13 -0500

    MVC tutorial - Add a new field
    https://learn.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/new-field?view=aspnetcore-9.0&tabs=visual-studio-code

--- MvcMovie/Migrations/20251103053615_InitialCreate.Designer.cs
+++ MvcMovie/Migrations/20251103053615_InitialCreate.Designer.cs
@@ -11,7 +11,7 @@ using MvcMovie.Data;
 namespace MvcMovie.Migrations
 {
     [DbContext(typeof(MvcMovieContext))]
-    [Migration("20251102223842_InitialCreate")]
+    [Migration("20251103053615_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
@@ -30,6 +30,9 @@ namespace MvcMovie.Migrations
                         .HasColumnType("TEXT");
 
                     b.Property<decimal>("Price")
+                        .HasColumnType("decimal(18, 2)");
+
+                    b.Property<string>("Rating")
                         .HasColumnType("TEXT");
 
                     b.Property<DateTime>("ReleaseDate")
