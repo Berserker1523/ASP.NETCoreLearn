682c218413db20ff6b31e03121dbfe6cd9263e13
Trollerker <berserker1523@hotmail.com>
2025-11-05 12:31:09 -0500

    SignalR with Typescript tutorial

---
+++ SignalRWebpack/Program.cs
@@ -0,0 +1,14 @@
+using SignalRWebpack.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.MapHub<ChatHub>("/hub");
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
