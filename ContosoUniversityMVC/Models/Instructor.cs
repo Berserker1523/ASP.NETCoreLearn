4686b135670c53e9532b774ad247ae1766a5bdeb
Trollerker <berserker1523@hotmail.com>
2025-11-16 22:03:25 -0500

    EF Core with MVC tutorial - Inheritance
    https://learn.microsoft.com/en-us/aspnet/core/data/ef-mvc/inheritance?view=aspnetcore-9.0

--- ContosoUniversityMVC/Models/Instructor.cs
+++ ContosoUniversityMVC/Models/Instructor.cs
@@ -1,30 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ContosoUniversity.Models;
 
-public class Instructor
+public class Instructor : Person
 {
-    public int ID { get; set; }
-
-    [Required]
-    [Display(Name = "Last Name")]
-    [StringLength(50)]
-    public required string LastName { get; set; }
-
-    [Required]
-    [Column("FirstName")]
-    [Display(Name = "First Name")]
-    [StringLength(50)]
-    public required string FirstMidName { get; set; }
 
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [Display(Name = "Hire Date")]
     public DateTime HireDate { get; set; }
-
-    [Display(Name = "Full Name")]
-    public string FullName => LastName + ", " + FirstMidName;
 
     public ICollection<CourseAssignment> CourseAssignments { get; set; } = new HashSet<CourseAssignment>();
 
--- ContosoUniversityMVC/Models/Student.cs
+++ ContosoUniversityMVC/Models/Student.cs
@@ -1,29 +1,13 @@
-﻿namespace ContosoUniversity.Models;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class Student
+namespace ContosoUniversity.Models;
+
+public class Student : Person
 {
-    public int ID { get; set; }
-
-    [Required]
-    [StringLength(50, MinimumLength = 2)]
-    [Display(Name = "Last Name")]
-    public string LastName { get; set; }
-
-    [Required]
-    [StringLength(50, MinimumLength = 2)]
-    [Column("FirstName")]
-    [Display(Name = "First Name")]
-    public string FirstMidName { get; set; }
-
     [DataType(DataType.Date)]
     [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
     [Display(Name = "Enrollment Date")]
     public DateTime EnrollmentDate { get; set; }
-
-    [Display(Name = "Full Name")]
-    public string FullName => LastName + ", " + FirstMidName;
 
     public ICollection<Enrollment> Enrollments { get; set; }
 }
